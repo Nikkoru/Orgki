365759766e4bc46c8c0425e453071a07a07bd375
Nicolas Godoy <nikobenja.gv@gmail.com>
2026-08-23 15:34:49 -0400

    Moved some functions & added getters for m_Activities and m_Tables

--- include/libOrgki/Plan.hpp
+++ include/libOrgki/Plan.hpp
@@ -18,6 +18,8 @@ public:
     Table& GetTable(TableID id);
     void SetTable(TableID id, Table& newTable);
     bool TableExists(TableID id) { return m_Tables.contains(id); }
+    size_t GetTableCount() { return m_Tables.size(); }
+    std::map<TableID, Table>& GetTables() { return m_Tables; };
 
     void CreateActivity(Activity& act);
     void CreateActivity(const std::string& name, const std::string& desc = "", bool isDynamic = false);
@@ -25,12 +27,9 @@ public:
     Activity& GetActivity(ActivityID id);
     void SetActivity(ActivityID id, Activity& newAct);
     bool ActivityExists(ActivityID id) { return m_Activities.contains(id); }
+    size_t GetActivityCount() { return m_Activities.size(); }
+    std::map<ActivityID, Activity>& GetActivities() { return m_Activities; };
 
     void AddActivityToTable(TableID tableID, ActivityID actID, TimeRange range);
-
-    size_t GetActivityCount() { return m_Activities.size(); }
-
-    size_t GetTableCount() { return m_Tables.size(); }
-    std::map<TableID, Table>& GetTables() { return m_Tables; };
 };
 
