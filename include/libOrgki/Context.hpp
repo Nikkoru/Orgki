092832ac56d4a30f018f279a3b48310ff93287b9
Nicolas Godoy <nikobenja.gv@gmail.com>
2026-09-06 20:10:40 -0300

    Adding Lua for extensibility and plugin support

---
+++ include/libOrgki/Context.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Commands/Parser.hpp"
+#include "Settings/SettingsManager.hpp"
+#include "PlanManager.hpp"
+#include <filesystem>
+#include <lua.hpp>
+
+namespace Orgki {
+struct ContextInitSettings{
+    bool initLua{ true };
+    bool luaOpenLibs{ true };
+    bool loadPlugins{ true };
+    bool addBuiltInCmds{ true };
+    bool loadSettings{ true };
+    std::filesystem::path settingsPath{};
+};
+
+class Context {
+public:
+    lua_State* luaState{};
+    SettingsManager settingsMgr{};
+    PlanManager planMgr{};
+    Parser parser{};
+private:
+    Command::StatusData _BuiltinLuaCallback(std::vector<std::string>& args);
+    Command::StatusData _BuiltinLuaSourceCallback(std::vector<std::string>& args);
+public:
+    Context() : Context(ContextInitSettings{}) {}
+    Context(ContextInitSettings settings);
+    ~Context();
+
+    void GetLastStatus();
+};
+}
---
+++ lib/libOrgki/Context.cpp
@@ -0,0 +1,231 @@
+#include "Context.hpp"
+#include <lauxlib.h>
+
+// namespace {
+// void AddBuiltInCommands(Orgki::Parser& parser) {
+//     parser.AddCommandBulk({
+//         // {
+//         //     .command = "lua",
+//         //     .description = "Evaluates the next argument as Lua",
+//         //     .usage = "[LUA_CODE]",
+//         //     .minArgs = 1,
+//         //     .maxArgs = 1,
+//         //     .callback = luaCallback
+//         // },
+//         // {
+//         //     .command = "luaSource",
+//         //     .description = "Evaluates or sources the given Lua file",
+//         //     .usage = "[FILE]",
+//         //     .minArgs = 1,
+//         //     .maxArgs = 1,
+//         //     .callback = luaSourceCallback
+//         // },
+//         {
+//             .command = "createTable",
+//             .description = "",
+//             .usage = "[NAME]",
+//             .minArgs = 1,
+//             .maxArgs = 1,
+//             .callback = createTableCallback
+//         },
+//         {
+//             .command = "seeTables",
+//             .description = "",
+//             .usage = "",
+//             .minArgs = 0,
+//             .maxArgs = 0,
+//             .callback = seeTablesCallback
+//         },
+//         {
+//             .command = "editTable",
+//             .description = "",
+//             .usage = "[ID] [ATTRIBUTE] <VALUE>",
+//             .minArgs = 2,
+//             .maxArgs = 3,
+//             .callback = editTableCallback
+//         },
+//         {
+//             .command = "getTable",
+//             .description = "",
+//             .usage = "[ID]",
+//             .minArgs = 1,
+//             .maxArgs = 1,
+//             .callback = getTableCallback
+//         },
+//         {
+//             .command = "addActivityToTable",
+//             .description = "[ACTIVITY_ID] [TABLE_ID] <TIME_RANGE>",
+//             .usage = "",
+//             .minArgs = 2,
+//             .maxArgs = 3,
+//             .callback = addActivityToTableCallback
+//         },
+//         {
+//             .command = "seeActivities",
+//             .description = "",
+//             .usage = "",
+//             .minArgs = 0,
+//             .maxArgs = 0,
+//             .callback = seeActivitiesCallback
+//         },
+//         {
+//             .command = "createActivity",
+//             .description = "",
+//             .usage = "[NAME] <DESCRIPTION> <\"dynamic\">",
+//             .minArgs = 1,
+//             .maxArgs = 3,
+//             .callback = createActivityCallback
+//         },
+//         {
+//             .command = "editActivity",
+//             .description = "",
+//             .usage = "[ID] [ATTRIBUTE] <VALUE>",
+//             .minArgs = 2,
+//             .maxArgs = 3,
+//             .callback = editActivityCallback
+//         },
+//         {
+//             .command = "whatOn",
+//             .description = "WIP",
+//             .usage = "[TIME / TIME_RANGE] <TABLE>",
+//             .minArgs = 1,
+//             .maxArgs = 2,
+//             .callback = whatOnCallback
+//         },
+//         {
+//             .command = "addSetting",
+//             .description = "Adds a setting based on the given key and optional default value",
+//             .usage = "[SETTING_KEY] <VALUE>",
+//             .minArgs = 1,
+//             .maxArgs = 2,
+//             .callback = addSettingCallback
+//         },
+//         {
+//             .command = "setSetting",
+//             .description = "Sets a setting based on the given key and value",
+//             .usage = "[SETTING_KEY] [VALUE]",
+//             .minArgs = 2,
+//             .maxArgs = 2,
+//             .callback = setSettingCallback
+//         },
+//         {
+//             .command = "removeSetting",
+//             .description = "Removes the setting on the given key",
+//             .usage = "[SETTING_KEY]",
+//             .minArgs = 1,
+//             .maxArgs = 1,
+//             .callback = removeSettingCallback
+//         },
+//         {
+//             .command = "getSetting",
+//             .description = "Gets a setting value based on the given key",
+//             .usage = "[SETTING_KEY]",
+//             .minArgs = 1,
+//             .maxArgs = 1,
+//             .callback = getSettingCallback
+//         },
+//         {
+//             .command = "getAllSettings",
+//             .description = "Gets all the settings",
+//             .usage = "",
+//             .minArgs = 0,
+//             .maxArgs = 0,
+//             .callback = getAllSettingsCallback
+//         },
+//     });
+// }
+// }
+
+namespace Orgki {
+Command::StatusData Context::_BuiltinLuaCallback(std::vector<std::string>& args) {
+    const std::string ASCII_RESET       { "\x1b[0m" };
+    const std::string ASCII_RED_FG      { "\x1b[31m" };
+    auto code = args[0];
+
+    auto status = luaL_dostring(luaState, code.c_str());
+
+    if (status != LUA_OK) {
+        auto errStr = lua_tostring(luaState, -1);   
+        return {
+            .msg = std::format("{}Lua failed to execute: {}{}", ASCII_RED_FG, ASCII_RESET, errStr),
+            .status = Orgki::Command::Status::FAILED
+        };
+    }
+
+    return {
+        .msg = "evaluated",
+        .status = Orgki::Command::Status::OK
+    };
+}
+
+Command::StatusData Context::_BuiltinLuaSourceCallback(std::vector<std::string>& args) {
+    const std::string ASCII_RESET       { "\x1b[0m" };
+    const std::string ASCII_RED_FG      { "\x1b[31m" };
+
+    auto code = args[0];
+
+    if (!std::filesystem::exists(code)) return {
+        .msg = std::format("{}file not found: {}\"{}\"", ASCII_RED_FG, ASCII_RESET, code),
+        .status = Orgki::Command::Status::INVALID_ARGS
+    };
+
+    auto status = luaL_dofile(luaState, code.c_str());
+
+    if (status != LUA_OK) {
+        auto errStr = lua_tostring(luaState, -1);   
+        return {
+            .msg = std::format("{}Lua failed to execute: {}{}", ASCII_RED_FG, ASCII_RESET, errStr),
+            .status = Orgki::Command::Status::FAILED
+        };
+    }
+    
+    return {
+        .msg = "evaluated",
+        .status = Orgki::Command::Status::OK
+    };
+}
+
+Context::Context(ContextInitSettings settings) {
+    if (settings.initLua) {
+        luaState = luaL_newstate();
+        if (settings.luaOpenLibs)
+            luaL_openlibs(luaState);
+    }
+
+    if (settings.addBuiltInCmds) {
+        settingsMgr._AddCommands(parser);
+        if (settings.initLua) {
+            parser.AddCommandBulk({
+                {
+                    .command = "lua",
+                    .description = "Evaluates the next argument as Lua",
+                    .usage = "[LUA_CODE]",
+                    .minArgs = 1,
+                    .maxArgs = 1,
+                    .callback = Command::Callback{ std::bind(&Context::_BuiltinLuaCallback, this, std::placeholders::_1) }
+                },
+                {
+                    .command = "luaSource",
+                    .description = "Evaluates or sources the given Lua file",
+                    .usage = "[FILE]",
+                    .minArgs = 1,
+                    .maxArgs = 1,
+                    .callback = Command::Callback{ std::bind(&Context::_BuiltinLuaSourceCallback, this, std::placeholders::_1) } 
+                },
+            });
+
+            if (settings.loadSettings) {
+                if (settings.settingsPath.empty()) {
+                    settings.settingsPath = "settings.json";
+                }
+
+                settingsMgr.LoadSettings(settings.settingsPath);
+            }
+        }
+    }
+}
+
+Context::~Context() {
+    lua_close(luaState);
+}
+}
